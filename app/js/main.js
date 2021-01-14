449046bb5cd96f4da07b7da8571b2debce38ce5f
serhii1991 <serhiikleimovych@gmail.com>
2021-01-14 21:31:59 +0100

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,49 @@ $(function() {
 		centerMode: true,
 		autoplay: true,
   	autoplaySpeed: 3000,
-  	arrows: false
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1701,
+				settings: {
+					slidesToShow: 5
+				}
+			},
+			{
+				breakpoint: 1441,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 1130,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 891,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 660,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					destroy: 'slick'
+				}
+			}
+		]
+	});
+
+	$('.header__menu-btn').on('click', function() {
+		$('.header__menu-list').slideToggle();
 	});
 
 	var mixer = mixitup('.dishes__content');
